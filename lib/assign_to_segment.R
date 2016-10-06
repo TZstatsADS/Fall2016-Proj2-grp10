9931d0c10a86a8285739a6c086d822ad7b493c13
alex-saez <as3171@columbia.edu>
2016-10-06 14:39:32 -0400

    small update

--- lib/assign_to_segment.R
+++ lib/assign_to_segment.R
@@ -1,36 +1,33 @@
 
 
-library(data.table)
-library(dplyr)
-
-dist_to_segment = function(point, line){
-  # auxiliary function to calculate distance between a point and a bounded line segment,
-  # where point in the form c(x0,y0) and line in the form c(x1,y1,x2,y2)
-  
-  x0 = point[1]; y0 = point[2]
-  x1 = line[1]; y1 = line[2]
-  x2 = line[3]; y2 = line[4]
-  
-  px = x2-x1
-  py = y2-y1
-  u = ((x0 - x1)*px + (y0 - y1)*py) / (px^2 + py^2)
-  if(u>1) u = 1
-  else if(u<0) u = 0
-  x = x1 + u*px
-  y = y1 + u*py
-  dx = x - x0
-  dy = y - y0
-  dist = dx^2 + dy^2
-    
-  return(dist)
-}
-
-
 assign_to_segment = function(point, node_list){
   # function to assign a feature with (lon, lat) coords to the street segment it belongs to on the map
   # point in the form c(lon, lat)
   # need to preload the list of all nodes and give it as second argument: node_list = fread('../output/node_list.csv')
   # returns the ID of the assigned segment. IDs are defined by table 'street blocks.csv'
+  
+  library(data.table)
+  library(dplyr)
+  
+  dist_to_segment = function(point, line){
+    # auxiliary function to calculate distance between a point and a bounded line segment,
+    # where point in the form c(x0,y0) and line in the form c(x1,y1,x2,y2)
+    x0 = point[1]; y0 = point[2]
+    x1 = line[1]; y1 = line[2]
+    x2 = line[3]; y2 = line[4]
+    px = x2-x1
+    py = y2-y1
+    u = ((x0 - x1)*px + (y0 - y1)*py) / (px^2 + py^2)
+    if(u>1) u = 1
+    else if(u<0) u = 0
+    x = x1 + u*px
+    y = y1 + u*py
+    dx = x - x0
+    dy = y - y0
+    dist = dx^2 + dy^2
+    return(dist)
+  }
+  
   
   node_coords = as.matrix(select(node_list, lon, lat))
   
@@ -58,7 +55,7 @@ assign_to_segment = function(point, node_list){
 
 
 # # for checking: pick a tree and plot its location along as the 2 end points of the segment it was assigned to:
-#
+# 
 # library(leaflet)
 # blocks = fread('../data/street blocks.csv')
 # node_list = fread('../output/node_list.csv')
@@ -66,7 +63,7 @@ assign_to_segment = function(point, node_list){
 # trees = as.matrix(select(trees, longitude, latitude))
 # n_nodes = nrow(node_list)
 # 
-# point = trees[152254,] # enter any tree index here
+# point = trees[154454,] # enter any tree index here
 # seg = select(filter(blocks, PHYSICALID==assign_to_segment(point, node_list)), the_geom)
 # seg_coords = nodes_from_seg(seg) # find this function within /lib/build_node_list.R
 # 
