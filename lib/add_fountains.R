bc066a46d923f91e90e00ba08decf2cadc5d3c89
Alex Saez <as3171@columbia.edu>
2016-10-09 14:19:14 -0400

    code for adding water fountains to segments

--- lib/add_fountains.R
+++ lib/add_fountains.R
@@ -1,54 +1,57 @@
 
-# code for assigning water fountains to Manhattan street segment.
+# Code for assigning water fountains to Manhattan street segments.
 
 library(data.table)
 library(dplyr)
 library(mgcv)
 
-trees = fread('../data/TreesCount2015Trees.csv')
+fountains = fread('../data/drink_location.csv')
 blocks = fread('../output/blocks_Manhattan.csv')
 node_list = fread('../output/node_list.csv')
 manhattan_outline = fread('../data/Boundary.csv')
 source('../lib/assign_to_segment.R')
 
-trees = select(trees, lon=longitude, lat=latitude) # remove useless columns
-trees = as.matrix(trees)
+fountains = fountains %>%
+              select(lon, lat) %>%
+              filter(!is.na(lon) & !is.na(lat)) %>%
+              as.matrix()
 manhattan_outline = as.matrix(manhattan_outline)
-trees = trees[in.out(manhattan_outline, trees),] # select manhattan trees only
+fountains = fountains[in.out(manhattan_outline, fountains),] # select manhattan fountains only
 node_list = filter(node_list, segment_id %in% blocks$PHYSICALID) # manhattan nodes only
 
 
-ptm <- proc.time()
-a = apply(trees[1:100,], 1, FUN=assign_to_segment, node_list)
-proc.time() - ptm  
-  
-# add lon and lat of segment end points as columns:
-trees = mutate(trees, 
-                start_lon = sapply(the_geom, get_end_nodes,'start_lon'),
-                start_lat = sapply(the_geom, get_end_nodes,'start_lat'),
-                end_lon = sapply(the_geom, get_end_nodes,'end_lon'),
-                end_lat = sapply(the_geom, get_end_nodes,'end_lat') )
+# assign a segment ID to each tree:
+segments_fountains = apply(fountains, 1, FUN=assign_to_segment, node_list)
 
+# count fountains per segment:
+fountains_per_seg = data.frame(table(segments_fountains))
 
+fountains_per_seg = fountains_per_seg %>%
+                    select(seg_ID = segments_fountains, n_fountains = Freq) %>%
+                    mutate(seg_ID = as.integer(as.character(seg_ID)))
 
-# add unique IDs to segment end points based on their coords rounded to the 5th decimal:
-blocks = blocks %>% 
-          mutate(start_ID = paste(as.character(round(start_lon, 5)), as.character(round(start_lat, 5)))) %>% 
-          mutate(end_ID = paste(as.character(round(end_lon, 5)), as.character(round(end_lat, 5))))
+blocks = left_join(blocks, fountains_per_seg, by=c('PHYSICALID'='seg_ID'))
+
+blocks$n_fountains[is.na(blocks$n_fountains)] = 0 # replace NAs with 0s
+
+blocks$n_fountains[blocks$PHYSICALID %in% c(132284, 3763)] = 2 # correct numbers that don't make sense based on map
 
 
 write.csv(blocks, file = '../output/blocks_Manhattan.csv', row.names = FALSE)
 
 
-# # for checking that it works:
-# library(leaflet)
-# block = filter(blocks, PHYSICALID==3188) # enter any block ID here
-# 
-# m <- leaflet() %>%
-#       addTiles() %>%
-#       addMarkers(lng=block$start_lon , lat=block$start_lat) %>%
-#       addMarkers(lng=block$end_lon , lat=block$end_lat)
-# m
-
-
+# for checking that it works: fountains in red, assigned segments in blue
+library(leaflet)
+segs = which(blocks$n_fountains!=0)
+m <- leaflet() %>% addTiles()
+for(i in segs){
+  m = addPolylines(m, lng = c(blocks$start_lon[i], blocks$end_lon[i]),
+                   lat = c(blocks$start_lat[i], blocks$end_lat[i]),
+                   opacity = 1
+  )
+}
+for(i in 1:nrow(fountains)){
+  m = addCircles(m, lng = c(fountains[i,1]), lat = c(fountains[i,2]), color = 'red', opacity = 1)
+}
+m
 
