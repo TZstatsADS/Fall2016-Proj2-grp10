8444aa9530e5c22fb176cc373fe301618618a9a9
caz2114 <caz2114@barnard.edu>
2016-10-10 20:39:36 -0400

    UI update

--- doc/Ui.R
+++ doc/Ui.R
@@ -10,75 +10,79 @@ vars <- c(
   "Subway")
 # Choices for drop-downs
 shinyUI(
-
-
-
-navbarPage("Jogger", id="nav",
-           
-           tabPanel(tags$em("Jogger's map"),
-                    div(class="outer",
-                        
-                        tags$head(
-                          # Include our custom CSS
-                          includeCSS("styles.css"),
-                          includeScript("gomap.js")
+  
+  
+  navbarPage("Jogger", id="nav",
+             
+             tabPanel(tags$em("Jogger's map"),
+                      div(class="outer",
+                          
+                          tags$head(
+                            # Include our custom CSS
+                            includeCSS("styles.css"),
+                            includeScript("gomap.js")
+                          ),
+                          
+                          leafletOutput("map", width="100%", height="100%"),
+                          
+                          # Shiny versions prior to 0.11 should use class="modal" instead.
+                          absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                        width = 330, height = "auto",
+                                        
+                                        h2("Jogger's path"),
+                                        textInput("start",label='Where you start?',value = "columbia university, new york"),
+                                        textInput("stop",label='Where you stop?',value = "times square, new york"),
+                                        checkboxInput("return", label = "Same returning route"),
+                                        
+                                        #changed layout to match input of code 
+                                        sliderInput("distance", label = "Distance: ", min = 0, max = 15, value = 7.5),
+                                        sliderInput("tree", "Tree:", min=0, max=100, value=50),
+                                        sliderInput("slope", label = "Slope", min=0, max=10, value=5),
+                                        sliderInput("foutain", label = "Drinking Foutain: ", min=0, max=10, value=5),
+                                        sliderInput("restroom", label = "Restroom", min=0, max=10, value=5),
+                                        
+                                        submitButton("Update"),
+                                        # if we need to add some initial plot here
+                                        hr()
+                                        
+                                        # plotOutput(),
+                                        # plotOutput()
+                          ),
+                          ## where our data come from and copyright?
+                          tags$div(id="cite",
+                                   'Data compiled for ', tags$em('New York Opensource Data'), ' until 2016'
+                          )
+                      )
+             ),
+             
+             tabPanel(tags$em("Path conditions"),
+                      fluidRow(
+                        column(3,
+                               selectInput("states", "States", choices = c('a','b'), selected = 'a', multiple=TRUE)
                         ),
-                        
-                        leafletOutput("map", width="100%", height="100%"),
-                        
-                        # Shiny versions prior to 0.11 should use class="modal" instead.
-                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                      draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                      width = 330, height = "auto",
-                                      
-                                      h2("Jogger's path"),
-                                      textInput("start",label='Where you start?',value = "columbia university, new york"),
-                                      textInput("stop",label='Where you stop?',value = "times square, new york"),
-                                      selectInput("pre1", label = "1st Preference", choices = vars,selected='River'),
-                                      selectInput("pre2", label = "2nd Preference", choices = vars, selected = "Subway"),
-                                      selectInput("pre3", label = "3rd Preference", choices = vars, selected = 'Tree'),
-                                      selectInput("pre4", label = "4rd Preference", choices = vars, selected = 'River'),
-                                      selectInput("pre5", label = "5rd Preference", choices = vars, selected = 'Toilet')
-    
-
-                                     # if we need to add some initial plot here
-                                     # plotOutput(),
-                                     # plotOutput()
+                        column(3,
+                               selectInput("input.states", "fasfas",choices = c('dasd','ff','sfaas'),selected = 'dasd',
+                                           multiple=TRUE)
                         ),
-                        ## where our data come from and copyright?
-                        tags$div(id="cite",
-                                 'Data compiled for ', tags$em('New York Opensource Data'), ' until 2016'
+                        column(3,
+                               selectInput("input.states","asdadsa", choices = c('sda','dasd','ff'),selected = 'sda',multiple = TRUE
+                               )
                         )
-                    )
-           ),
-           
-           tabPanel(tags$em("Path conditions"),
-                    fluidRow(
-                      column(3,
-                             selectInput("states", "States", choices = c('a','b'), selected = 'a', multiple=TRUE)
                       ),
-                      column(3,
-                             selectInput("input.states", "fasfas",choices = c('dasd','ff','sfaas'),selected = 'dasd',
-                                              multiple=TRUE)
+                      fluidRow(
+                        column(1,
+                               numericInput("minScore", "Min score", min=0, max=100, value=0)
+                        ),
+                        column(1,
+                               numericInput("maxScore", "Max score", min=0, max=100, value=100)
+                        )
                       ),
-                      column(3,
-                             selectInput("input.states","asdadsa", choices = c('sda','dasd','ff'),selected = 'sda',multiple = TRUE
-                             )
-                      )
-                    ),
-                    fluidRow(
-                      column(1,
-                             numericInput("minScore", "Min score", min=0, max=100, value=0)
-                      ),
-                      column(1,
-                             numericInput("maxScore", "Max score", min=0, max=100, value=100)
-                      )
-                    ),
-                    hr()
-                    # show the conditions here:
-                    #DT::dataTableOutput("ziptable")
-           )
-           
-           #conditionalPanel("false", icon("crosshair"))
-)
+                      hr()
+                      # show the conditions here:
+                      #DT::dataTableOutput("ziptable")
+             )
+             
+             #conditionalPanel("false", icon("crosshair"))
+  )
 )
