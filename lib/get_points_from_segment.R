94bb4b9f07f1fa3d2e22487ac3408c05f4c4596f
alex-saez <as3171@columbia.edu>
2016-10-11 10:43:46 -0400

    function for extracting intermediary points for plotting segments

--- lib/get_points_from_segment.R
+++ lib/get_points_from_segment.R
@@ -25,18 +25,18 @@ get_points_from_segment = function(the_geom){
 
 
 
-# Usage:
-library(leaflet)
-blocks = fread('../output/blocks_Manhattan.csv')
-
-ind = sample(nrow(blocks), 1000)
-
-m <- leaflet() %>% addTiles()
-for(i in ind){
-  seg_points = get_points_from_segment(blocks$the_geom[i])
-  m = addPolylines(m, lng = seg_points$lon, lat = seg_points$lat,   opacity = 1)
-}
-m
+# # Usage:
+# library(leaflet)
+# blocks = fread('../output/blocks_Manhattan.csv')
+# 
+# ind = sample(nrow(blocks), 1000)
+# 
+# m <- leaflet() %>% addTiles()
+# for(i in ind){
+#   seg_points = get_points_from_segment(blocks$the_geom[i])
+#   m = addPolylines(m, lng = seg_points$lon, lat = seg_points$lat,   opacity = 1)
+# }
+# m
 
 
 
