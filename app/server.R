4fc9683a2b91b978359c38c2f66e90c9c188a9c7
alex-saez <as3171@columbia.edu>
2016-10-12 12:02:26 -0400

    updated so that it runs on any computer

--- app/server.R
+++ app/server.R
@@ -1,114 +1,276 @@
 library(shiny)
-library(choroplethr)
-library(choroplethrZip)
+library(leaflet)
+library(lattice)
 library(dplyr)
+library(ggplot2)
+library(RCurl)
+library(RJSONIO)
+library(plyr)
+library(ggplot2)
+library(plotly)
+load("../output/Nodes.RData")
+load("../output/Segments.RData")
+load("../output/Original.Segments.RData")
+source("../lib/Main_Algo_Function.R")
+source("../lib/Main_Algo_Code2.R")
+source("../lib/get_points_from_segment.R")
+blocks = fread('../output/blocks_Manhattan.csv')
+restrooms = fread('../data/restroom_coordinates.csv')
+fountains = fread('../data/drink_location.csv')
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", "Chelsea and Clinton",
-           "East Harlem", "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", "Lower Manhattan",
-           "Lower East Side", "Upper East Side", "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
 
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  # _____________________________________________API: geocode______________________________________________#
+  #https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC82ht4goSYy9M7Dp9tXc-vO9qxCoeF0jM'
+  # Geocode API
+  url <- function(address, return.call = "json", sensor = "false") {
+    root <- "https://maps.google.com/maps/api/geocode/"
+    u <- paste(root, return.call, "?address=", address,"&key=AIzaSyC82ht4goSYy9M7Dp9tXc-vO9qxCoeF0jM", sep = "")
+    return(URLencode(u))
+  }
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
+  geoCode <- function(address,verbose=FALSE) {
+    if(verbose) cat(address,"\n")
+    u <- url(address)
+    doc <- getURL(u) #download JSON 
+    x <- fromJSON(doc,simplify = FALSE) # convert JSON into list
+    # check is the status is ok
+    if(x$status=="OK") {
+      lat <- as.numeric(x$results[[1]]$geometry$location$lat)
+      lng <- as.numeric(x$results[[1]]$geometry$location$lng)
+      location_type <- x$results[[1]]$geometry$location_type
+      formatted_address <- x$results[[1]]$formatted_address
+      return(c(lat, lng, location_type, formatted_address))
+    } else {
+      return(c(NA,NA,NA,NA))
+    }
+  }
+  
+  
+  #__________________________________ICON______________________________________________________#
+  # make icon
+  start <- makeIcon(
+    iconUrl = "../data/super_mario.png",
+    iconWidth = 25, iconHeight = 40
+  )
+  
+  end = makeIcon(
+    iconUrl = "../data/princess.jpg",
+    iconWidth = 25, iconHeight = 40
+  )
+  
+  toilet_icon=makeIcon(
+    iconUrl = "../data/toilet.png",
+    iconWidth = 25, iconHeight = 30
+  )
+  
+  fountain_icon=makeIcon(
+    iconUrl = "../data/fountain.png",
+    iconWidth = 25, iconHeight = 30
+  )
+  
+  
+  
+  #____________________________________________MAP______________________________________________________#
+  
+  
+  #functions to create markers and polylines
+  showRoutine <- function(lng,lat,col) {
+    leafletProxy("map") %>% addPolylines(lng, lat, color = col)
+  }
+  
+  showRestroom<-function(lng,lat,restroom){
+    leafletProxy("map") %>% addMarkers(lng,lat,icon = toilet_icon)
+  }
+  
+  showDrink<-function(lng,lat,drink){
+    leafletProxy("map") %>% addMarkers(lng,lat,icon = fountain_icon)
+  } 
+  
+  #Creating paths for map
+  observe({
+    if(input$end_dis == 1){
+      leafletProxy("map") %>% clearShapes()
+      event <- Find.Path(input$start,input$tree,input$slope,
+                         input$foutain,input$restroom,input$width,Nodes,Segments,
+                         Original.Segments,
+                         NA,input$stop,input$return)
+    }else{
+      leafletProxy("map") %>% clearShapes()
+      event <- Find.Path(input$start,input$tree,input$slope,
+                         input$foutain,input$restroom,input$width,Nodes,Segments,
+                         Original.Segments,
+                         input$distance,NA,input$return)
+    }
+    #inital run
+    isolate({
+      showRoutine(lng=as.vector(event$Intersection.Go$Longtitude),
+                  lat=as.vector(event$Intersection.Go$Latitude),
+                  col = "#FF0088")
+    })
+    
+    #return run
+    isolate({
+      showRoutine(lng=as.vector(event$Intersection.Back$Longtitude),
+                  lat=as.vector(event$Intersection.Back$Latitude),
+                  col = "#5500FF")
+    })
+    
+    output$summary_text <- renderPrint(
+      paste("The total distance is ", event$Length,"kilometers.")
+    )
   })
   
-  ## Panel 1: summary plots of time trends, unit price and full price of sales. 
-  output$distPlot <- renderPlot({
+  #Display bathroom and fountains on path only
+  observe({
+    if(input$RP_layer || input$FO_layer){
+
+      leafletProxy("map") %>% clearMarkers() %>%
+        addMarkers(data = points_start(),icon=start) %>%
+        addMarkers(data = points_end(),icon=end)
+      event <- Find.Path(input$start,input$tree,input$slope,
+                         input$foutain,input$restroom,input$width,Nodes,Segments,
+                         Original.Segments,
+                         NA,input$stop,input$return)
+
+      rl<-cbind(event$Edge$Longtitude1[which(event$Edge$Restroom>0)],
+                event$Edge$Latitude1[which(event$Edge$Restroom>0)],
+                event$Edge$Longtitude2[which(event$Edge$Restroom>0)],
+                event$Edge$Latitude1[which(event$Edge$Restroom>0)]
+      )
+      dl<-cbind(event$Edge$Longtitude1[which(event$Edge$Fountain>0)],
+                event$Edge$Latitude1[which(event$Edge$Fountain>0)],
+                event$Edge$Longtitude2[which(event$Edge$Fountain>0)],
+                event$Edge$Latitude1[which(event$Edge$Fountain>0)]
+      )
+
+      if(input$RP_layer == 1){
+        n<-nrow(rl)
+        rest_lat<-vector()
+        rest_lng<-vector()
+
+
+        for (i in 1:n == 1){
+          rest_lng[i]<-mean(rl[i,1],mean(c(rl[i,1],rl[i,3])))
+          rest_lat[i]<-mean(rl[i,2],mean(c(rl[i,2],rl[i,4])))
+        }
+
+        isolate({
+          showRestroom(lng = rest_lng, lat = rest_lat, restroom = restroom)
+        })
+
+      }
+
+
+      if(input$FO_layer == 1){
+        m<-nrow(dl)
+        drink_lat<-vector()
+        drink_lng<-vector()
+
+
+        for(i in 1:m){
+          drink_lng[i]<-mean(c(dl[i,1],dl[i,3]))
+          drink_lat[i]<-mean(c(dl[i,2],dl[i,4]))
+        }
+
+        isolate({
+          showDrink(lng=drink_lng,lat=drink_lat,drink =drink)
+        })
+      }
+    }
+
+  })
+  
+  # Find point from input
+  points_start <- eventReactive(input$start,{
+    address <- input$start
+    locations <- ldply(address,function(x) geoCode(x))
+    names(locations) <- c("lat", "lon", "location_type", "formatted")
+    return (cbind(locations$lon,locations$lat))
+  })
+  
+  points_end <- eventReactive(input$stop,{
+    address <- input$stop
+    locations <- ldply(address,function(x) geoCode(x))
+    names(locations) <- c("lat", "lon", "location_type", "formatted")
+    return (cbind(locations$lon,locations$lat))
+  })
+  
+  # Create map
+  output$map <- renderLeaflet({
+    if (input$end_dis == 1){
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>% setView(lng = -73.96411, lat =40.807722, zoom=17 ) %>%
+     
+      addMarkers(data = points_start(),icon=start) %>%
+      addMarkers(data = points_end(),icon=end)} else{
+          leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>% setView(lng = -73.96411, lat =40.807722, zoom=17 ) %>%
+     
+      addMarkers(data = points_start(),icon=start) 
+      }
+  })
+  
+
+  #_____________________________________________Factor Exploration___________________________________________#
+
+  # Create map
+  output$map2 <- renderLeaflet({
+      leaflet() %>%
+        addTiles(
+          urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+        ) %>% setView(lng = -73.96411, lat =40.807722, zoom=16 )
+  })
+
+  #Display bathroom and fountains
+  fountains = fountains %>%
+    select(lon, lat) %>%
+    filter(!is.na(lon) & !is.na(lat))
+  
+  observe({
+    leafletProxy("map2") %>% clearMarkers()%>%
+      addMarkers(data = points_start(),icon=start) %>% addMarkers(data = points_end(),icon=end)
+    if (input$RP_layer_all)
+      leafletProxy("map2") %>% addMarkers(lng = restrooms$LNG, lat = restrooms$LAT, icon = toilet_icon)
+    if (input$FO_layer_all)
+      leafletProxy("map2") %>% addMarkers(lng = fountains$lon, lat = fountains$lat, icon = fountain_icon)
+  })
+  
+  update_ind = reactive({
+    bounds = input$map2_bounds
+    with(blocks, which( (start_lon<bounds$east | end_lon<bounds$east) &
+                          (start_lon>bounds$west | end_lon>bounds$west) &
+                          (start_lat>bounds$south | end_lat>bounds$south) &
+                          (start_lat<bounds$north | end_lat<bounds$north) ) )
+  })
+  
+  #draw segments:
+  colors = colorRamp(c("red", "black", "green"))
+  
+  observe({
+    score = input$tree_pre*(blocks$tree_dens)  +  input$slope_pre*(-blocks$slope)
+    score = score[update_ind()]
+    score = rank(score)
     
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
+    leafletProxy("map2") %>% clearShapes()  
+    k = 0
+    for(i in update_ind()){
+      k = k + 1
+      seg_points = get_points_from_segment(blocks$the_geom[i])
+      leafletProxy("map2") %>% addPolylines(lng = seg_points$lon, 
+                                            lat = seg_points$lat,   
+                                            col = rgb(colors(score[k]/max(score))/255),
+                                            weight = 3, 
+                                            opacity = 1)
     }
     
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
   })
-  
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
-  })
+
 })
--- app/ui.r
+++ app/ui.r
@@ -1,40 +1,175 @@
 library(shiny)
+library(leaflet)
+library(DT)
+library(ggplot2)
+library(plotly)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+vars <- c(
+  "Tree",
+  "Drink Fountain",
+  "Toilet")
+dataset_selection <- c(
+  "Legnth",
+  "Slope",
+  "Tree",
+  "Drink Fountain",
+  "Toliet"
+)
+# Choices for drop-downs
+shinyUI(
   
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
   
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1")))
-    )
- )
-))
+  navbarPage(tags$img(tags$span("Jogger",class='edit'),src='https://maxcdn.icons8.com/Share/icon/Animals//running_rabbit1600.png',
+                      align = 'left',height = 40, width = 40,class = 'fixed'), 
+  id="nav",
+  
+  tabPanel(tags$em("Jogger's map"),
+           div(class="outer",
+               
+               tags$head(
+                 # Include our custom CSS
+                 includeCSS("styles.css"),
+                 includeScript("gomap.js")
+               ),
+               
+               leafletOutput("map", width="100%", height="100%"),
+               
+               # Shiny versions prior to 0.11 should use class="modal" instead.
+               absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                             draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                             width = 330, height = "auto",
+                             
+                             h2("Jogger's path"),
+                             textInput("start",label='Where you start?',value = "columbia university, new york"),
+                             
+                             # verbatimTextOutput("test"),
+                             radioButtons("end_dis", "Please choose ending location or distance",
+                                          c("end" = 1,"distance" = 2)),
+                             textInput("stop",label='Where you stop?(Optional)',value = "times square, new york"),
+                             numericInput("distance", label = "Distance: ", min = 0, max = 10, value = 5),
+                             
+                              
+                             
+          
+                             
+                             
+                             h5("Return using original path."),
+                             checkboxInput("return", label = "No"),
+                             
+                             #changed layout to match input of code 
+                             numericInput("tree", "Tree:", min=0, max=10, value=5),
+                             numericInput("slope", label = "Slope", min=0, max=10, value=5),
+                             numericInput("foutain", label = "Drinking Foutain: ", min=0, max=10, value=5),
+                             numericInput("restroom", label = "Restroom", min=0, max=10, value=5),
+                             numericInput("width", label = "Sidewalk Width", min = 0, max = 10, value = 5),
+                             
+                             submitButton("Update")
+                             # if we need to add some initial plot here
 
+               ),
+               
+               absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                             draggable = TRUE, top = 60, left = "auto", right = "auto", bottom = "auto",
+                             width = 330, height = "auto",
+                             
+                             h2("Path Information"),
+                             h4("The following is detailed information on your path."),
+                             
+                             h4("To view in greater detail, please select the following:"),
+                             checkboxInput("RP_layer","Restroom"),
+                             checkboxInput("FO_layer", "Foutain"),
+                             
+                             submitButton("Update"),
+                             
+                             h4("\nStatistics about the current course:"),
+                             verbatimTextOutput("summary_text")
+               ),
+ 
+               ## where our data come from and copyright?
+               tags$div(id="cite",
+                        'Data compiled for ', tags$em('New York Opensource Data'), ' until 2016'
+               )
+           )
+  ),
+
+  tabPanel(tags$em("Factor Exploration"),
+           tags$div(class='beauty',
+                    div(class="outer",
+                        leafletOutput("map2", width="100%", height="100%"),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = "auto", right = "auto", bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      h2("Path Information"),
+                                      h4("The following is detailed information on your path."),
+                                      
+                                      h4("To view in greater detail, please select the following:"),
+                                      checkboxInput("SP_TR", "Slope and Trees"),
+                                      numericInput("tree_pre", label = "   Trees:", min=1, max=100, value=50),
+                                      numericInput("slope_pre", label = "   Flatness:", min=1, max=100, value=50),
+                                      checkboxInput("RP_layer_all","Restroom"),
+                                      checkboxInput("FO_layer_all", "Fountain"),
+                                      
+                                      submitButton("Update")
+                                      )
+                    )
+           )
+  ),
+  
+  # tabPanel(tag$em("Data Sets"),
+  #          tags$div(class='beauty',
+  #                   fluidRow(
+  #                     selectInput("dataset","Please choose a dataset to display.", dataset_selection)
+  #                   ),
+  #                   
+  #                   fluidRow(
+  #                     DT::dataTableOutput("table")
+  #                   )
+  #           )
+  # 
+  # ),
+  
+  tabPanel(tags$em("About"),
+           tags$div(class='beauty',
+                    tags$h2("Our Team Members",class='trims'),
+                    tags$br(),
+                    tags$img(tags$ul( class = 'ulb',
+                                      tags$li(tags$span("Rong Li",class = 'forname'),class ='ulb'),
+                                      tags$li(tags$span('Alex Saez',class = 'forname'),class ='ulb'),
+                                      tags$li(tags$span('Catherine Zhao',class ='forname'),class ='ulb'),
+                                      tags$li(tags$span('Huilong An',class = 'forname'),class ='ulb'),
+                                      tags$li(tags$span('Ying Zhu',class ='forname'),class ='ulb'),
+                                      tags$br()
+                    ),
+                    src='http://orig04.deviantart.net/4c19/f/2013/358/c/f/coloured_sm3dw_character_icon_stamps_by_geno2925-d6z9hv6.png', width = 'AUTO',
+                    height = 'AUTO', class = 'fixed',align = 'center'),
+                    tags$br(),
+                    tags$hr(),
+                    tags$h2('About Our App',class='trims'),
+                    tags$p("This application is designed for New York joggers to explore their
+                           jogging path. It’s based on New York Open Data and allows users to 
+                           customize their preferences on trees, slopes, fountains and restrooms. 
+                           Furthermore, after input start location, the application has two modes
+                           for exploration process. One is to input the exact destination and the
+                           other one is to input the expect distance between destination and start 
+                           location. The app will automatically show the optimal jogging path according 
+                           to user’s preference along with the detail information of this path.",class='trimsP'),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br()
+           )
+           #conditionalPanel("false", icon("crosshair"))
+  )
+    
+)
+)
